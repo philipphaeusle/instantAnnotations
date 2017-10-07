0dec591e5e704ece3923b9ce37e0bc985232ca81
thibault <thibault.gerrier@student.uibk.ac.at>
2017-10-07 15:03:57 +0200

    lists all DS names, those with 'Simple' will be bold

--- javascript/instantAnnotations.js
+++ javascript/instantAnnotations.js
@@ -1,29 +1,35 @@
-
-
 function getAllDS(callback) { //return data
     $.ajax({
         type: "GET",
         url: 'https://semantify.it/api/domainSpecification',
         success: function (data) {
-          callback(data)
+            callback(data);
         },
-        error: function (data, xhr, status, err) {
+        error: function () {
+            console.log("error");
         }
     });
 }
 
-function getSpecificDS(){ //TODO call getAllDs and remove all without simple in front
-    getAllDS(function(data){
-      alert("one "+ data);
-      var allDS = JSON.parse(data);
-      alert("all "+allDS);
-    })
-  };
+function getSpecificDS() { //TODO call getAllDs and remove all without simple in front
+    getAllDS(function (data) {
+        for(var i in data){
+            if(data.hasOwnProperty(i)){
+                var name = data[i]["name"].toString();
+                if(name.indexOf("Simple") !== -1){
+                    $("<p><b>" + name + "</b></p>").appendTo($("#demo"));
+                }
+                else{
+                    $("<p>" + name + "</p>").appendTo($("#demo"));
+                }
+            }
+        }
+    });
+}
 
 
-
-function create(divID) { // TODO call getSpecificDs and create card for every object
+function create() { // TODO call getSpecificDs and create card for every object
     getSpecificDS();
-    $('<div>test1</div>').appendTo(divID);
-    $('<div>test2</div>').appendTo(divID);
+    $('<div>test1</div>').appendTo($("#demo"));
+    $('<div>test2</div>').appendTo($("#demo"));
 }
