b42c6bb78e4b0246d0afc254acd9c0353a95cc48
Philipp Häusle <philipp.haeusele@student.uibk.ac.at>
2017-10-07 14:37:50 +0200

    minor changes

--- javascript/instantAnnotations.js
+++ javascript/instantAnnotations.js
@@ -1,23 +1,29 @@
 
 
-function getAllDS() { //return data
+function getAllDS(callback) { //return data
     $.ajax({
         type: "GET",
         url: 'https://semantify.it/api/domainSpecification',
         success: function (data) {
-            return data;
-        }
+          callback(data)
+        },
         error: function (data, xhr, status, err) {
         }
     });
 }
 
-function getSpecificDs(){ //TODO call getAllDs and remove all without simple in front
+function getSpecificDS(){ //TODO call getAllDs and remove all without simple in front
+    getAllDS(function(data){
+      alert("one "+ data);
+      var allDS = JSON.parse(data);
+      alert("all "+allDS);
+    })
+  };
 
-}
 
 
 function create(divID) { // TODO call getSpecificDs and create card for every object
+    getSpecificDS();
     $('<div>test1</div>').appendTo(divID);
     $('<div>test2</div>').appendTo(divID);
 }
